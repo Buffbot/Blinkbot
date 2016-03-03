344e04e254b2207951c2a7c70981a99060f7b4cc
Scott Elwood <mechpants@gmail.com>
2016-03-03 00:51:52 -0800

    Create a common service for reusable bot helpers

---
+++ app/services/common.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  connection: Ember.inject.service('connection'),
+  modsList: null,
+
+  say(message) {
+    var channel = this.get('connection.config.channel');
+    this.get('connection.client').say(channel, message);
+  },
+
+  mentionSay(user, message) {
+    var display_name = user['display-name'] || user.get('display');
+    var message = `@${display_name}, ${message}`;
+
+    this.say(message);
+  },
+
+  updateModList() {
+    var current_user = this.get('connection.config.username');
+    var current_channel = this.get('connection.config.channel');
+    var modsList = [current_user];
+
+    var self = this;
+    this.get('connection').client.mods(current_channel).then(function(data) {
+      modsList = modsList.concat(data);
+      self.set('modsList', modsList);
+    });
+  },
+
+  isAdmin(username) {
+    return this.get('modsList').indexOf(username) >= 0;
+  }
+});
