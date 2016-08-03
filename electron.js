b27a15efe08f55d945d90f9bdda450511728cb37
Scott Elwood <mechpants@gmail.com>
2016-08-02 23:45:31 -0700

    Initial electron build

--- electron.js
+++ electron.js
@@ -3,6 +3,7 @@
 
 const electron             = require('electron');
 const path                 = require('path');
+const Menu                 = require('menu');
 const {app, BrowserWindow} = electron;
 const dirname              = __dirname || path.resolve(path.dirname());
 const emberAppLocation     = `file://${dirname}/dist/index.html`;
@@ -87,4 +88,26 @@ app.on('ready', function onReady() {
         console.log('This is a serious issue that needs to be handled and/or debugged.');
         console.log(`Exception: ${err}`);
     });
+
+    // Create the Application's main menu
+   var template = [{
+       label: "Application",
+       submenu: [
+           { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+           { type: "separator" },
+           { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+       ]}, {
+       label: "Edit",
+       submenu: [
+           { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+           { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+           { type: "separator" },
+           { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+           { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+           { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+           { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+       ]}
+   ];
+
+   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
